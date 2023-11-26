945d9bb7faed85ce5c7e91869892a529169f08ea
Gabriel S <gs.sanchez@uniandes.edu.co>
2023-11-25 20:15:25 -0500

    pruebas

--- KNN.py
+++ KNN.py
@@ -4,17 +4,20 @@ def moda(lst):
     return max(set(lst), key=lst.count)
 
 def euclidiana(punto, datos):
-    return np.sqrt(np.sum((punto - datos)**2, axis=1))
+    point = np.array(punto, dtype=float)
+    data = np.array(datos, dtype=float)
+
+    return np.sqrt(np.sum((point - data)**2, axis=1))
 
 class KClasificadorVecinosCercanos:
-    def __init__(self, k=10):
+    def __init__(self, k=11):
         self.k = k
 
-    def entrenar(self, X_entrenamiento, y_entrenamiento):
+    def fit(self, X_entrenamiento, y_entrenamiento):
         self.X_entrenamiento = X_entrenamiento
         self.y_entrenamiento = y_entrenamiento
   
-    def predecir(self, X_prueba):
+    def predict(self, X_prueba):
         vecinos = []
         for x in X_prueba:
             distancias = euclidiana(x, self.X_entrenamiento)
@@ -22,7 +25,12 @@ class KClasificadorVecinosCercanos:
             vecinos.append(y_estrella[:self.k])
         return list(map(moda, vecinos))
     
-    def puntaje(self, X_prueba, y_prueba):
-        y_pred = self.predecir(X_prueba)
+    def score(self, X_prueba, y_prueba):
+        y_pred = self.predict(X_prueba)
         precision = sum(y_pred == y_prueba) / len(y_prueba)
         return precision
+    
+    def get_params(self, deep=False):
+        return {
+            'k': self.k,
+        }
